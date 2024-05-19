fd1b6283c6c18e2899f22ff315e39bda51bc259d
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-05-19 18:14:25 +0200

    Install and setup tailwindcss

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,6 +1,7 @@
 import { themes as prismThemes } from 'prism-react-renderer';
 import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
+import tailwindPlugin from './plugins/tailwind-plugin.cjs';
 
 const config: Config = {
   title: 'Zekuru-v2',
@@ -28,6 +29,8 @@ const config: Config = {
     defaultLocale: 'en',
     locales: ['en'],
   },
+
+  plugins: [tailwindPlugin],
 
   presets: [
     [
